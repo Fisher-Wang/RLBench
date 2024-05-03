88190a41cf708edf19f447522c1e3c8ce03f032e
Fisher Wang <kingwfsh@163.com>
2024-05-03 23:04:56 +0800

    change file struction of collect_demo.py to OOP

--- tools/collect_demo.py
+++ tools/collect_demo.py
@@ -45,7 +45,7 @@ def write_json(path, data):
     with open(path, 'w+') as f:
         json.dump(data, f)
 
-def data_numpy_to_list(data: dict) -> dict:
+def ensure_numpy_as_list(data: dict) -> dict:
     '''
     Convert numpy arrays in nested dict or list to list
     data: nested dict or list, containing numpy arrays
@@ -54,12 +54,12 @@ def data_numpy_to_list(data: dict) -> dict:
         if type(v) == np.ndarray:
             data[k] = v.tolist()
         elif type(v) == dict:
-            data[k] = data_numpy_to_list(v)
+            data[k] = ensure_numpy_as_list(v)
         elif type(v) == list:
-            data[k] = [data_numpy_to_list(e) for e in v]
+            data[k] = [ensure_numpy_as_list(e) for e in v]
     return data
 
-def data_numpy_float32(data: dict) -> dict:
+def ensure_numpy_float32(data: dict) -> dict:
     '''
     Make sure all numpy arrays in nested dict or list have dtype float32
     data: nested dict or list, containing numpy arrays
@@ -68,9 +68,9 @@ def data_numpy_float32(data: dict) -> dict:
         if type(v) == np.ndarray:
             data[k] = v.astype(np.float32)
         elif type(v) == dict:
-            data[k] = data_numpy_float32(v)
+            data[k] = ensure_numpy_float32(v)
         elif type(v) == list:
-            data[k] = [data_numpy_float32(e) for e in v]
+            data[k] = [ensure_numpy_float32(e) for e in v]
     return data
 
 def xyzw_to_wxyz(quat: np.ndarray):
@@ -88,162 +88,210 @@ def float_array_to_str(arr: np.ndarray):
     return '[' + ', '.join([f'{e:.2f}' for e in arr]) + ']'
 
 ####################################
+## Demo writer
+####################################
+class DemoWriter:
+    def __init__(self, cfg: dict, file_path: str):
+        self.cfg = cfg
+        self.save_path = file_path
+        self.demo_entries : List[dict] = []
+    
+    def capture_env_setup_data(self, scene: Scene):
+        data_env_setup = {}
+        data_env_setup |= self._capture_panda_data(scene)
+        data_env_setup |= self._capture_object_data()
+        data_env_setup |= self._capture_joint_data()
+        
+        self.data_env_setup = data_env_setup
+    
+    def _capture_panda_data(self, scene: Scene):
+        init_arm_q = scene.robot.arm.get_joint_positions()
+        init_gripper_q = scene.robot.gripper.get_joint_positions()
+        init_q = np.concatenate([init_arm_q, init_gripper_q])
+        init_robot_pos = scene.robot.arm.get_position()
+        init_robot_quat = scene.robot.arm.get_quaternion()
+        init_robot_quat = xyzw_to_wxyz(init_robot_quat)
+        
+        robot_name = 'Panda'
+        robot = Object.get_object(robot_name)
+        init_panda_pos = robot.get_position()
+        init_panda_quat = robot.get_quaternion()
+        init_panda_quat = xyzw_to_wxyz(init_panda_quat)
+        
+        assert np.allclose(init_robot_pos, init_panda_pos)
+        assert np.allclose(init_robot_quat, init_panda_quat)
+        
+        print(f'[DEBUG] Initial panda q: {float_array_to_str(init_q)}')
+        print(f'[DEBUG] Initial panda position: {float_array_to_str(init_panda_pos)}')
+        print(f'[DEBUG] Initial panda orientation: {float_array_to_str(init_panda_quat)}')
+        
+        panda_data = {
+            'init_q': init_q,
+            'init_robot_pos': init_robot_pos,
+            'init_robot_quat': init_robot_quat,
+            'init_target_pos': init_robot_pos + np.array([20,20,20]),    # XXX
+            'init_target_quat': init_robot_quat,  # XXX
+        }
+        
+        return panda_data
+    
+    def _capture_object_data(self):
+        object_names = self.cfg['objects']
+        object_data = {}
+        for object_name in object_names:
+            object = Object.get_object(object_name)
+            init_object_pos = object.get_position()
+            init_object_quat = object.get_quaternion()
+            init_object_quat = xyzw_to_wxyz(init_object_quat)
+            object_data |= {
+                f'init_{object_name}_pos': init_object_pos,
+                f'init_{object_name}_quat': init_object_quat,
+            }
+            print(f'[DEBUG] Initial {object_name} position: {float_array_to_str(init_object_pos)}')
+            print(f'[DEBUG] Initial {object_name} orientation: {float_array_to_str(init_object_quat)}')
+        
+        return object_data
+    
+    def _capture_joint_data(self):
+        joint_names = self.cfg['joints']
+        joint_data = {}
+        for joint_name in joint_names:
+            joint = Joint(joint_name)
+            init_joint_q = joint.get_joint_position()
+            init_joint_q = np.array([init_joint_q])  # float to array
+            joint_data |= {
+                f'init_{joint_name}_q': init_joint_q,
+            }
+            print(f'[DEBUG] Initial {joint_name} q: {float_array_to_str(init_joint_q)}')
+        
+        return joint_data
+    
+    def save_demo_data(self, demo: Demo, task_name: str, variation_index: int):
+        data = {}
+
+        ## Process trajectory
+        qs = []
+        ee_acts = []
+        for obs in demo:
+            # collecting joint positions for q [L x q_len]
+            q = obs.joint_positions.tolist() + obs.gripper_joint_positions.tolist()
+            # collecting gripper open for ee_act [L x 1]
+            ee_act = [obs.gripper_open]
+            
+            qs.append(q)
+            ee_acts.append(ee_act)
+        
+        data_traj = {
+            'q': np.array(qs),
+            'ee_act': np.array(ee_acts),
+            'success': np.ones(len(qs), dtype=bool)
+        }
+        
+        ## All demo data
+        demo_entry = {
+            'name': f'{task_name}_traj_{variation_index}',
+            'asset_name': 'default_asset',
+            'episode_len': len(qs),
+            'env_setup': self.data_env_setup,
+            'robot_traj': data_traj
+        }
+    
+        self.demo_entries.append(demo_entry)
+    
+    def write(self):
+        max_episode_len = max([data_demo['episode_len'] for data_demo in self.demo_entries])
+        print(f'[INFO] Saving demos to {self.save_path}, max_episode_len: {max_episode_len}')
+        
+        full_data = {
+            'max_episode_len': max_episode_len,
+            'demos': {
+                'franka': self.demo_entries
+            }
+        }
+        
+        if self.save_path.endswith('.pkl'):
+            full_data = ensure_numpy_float32(full_data)
+            write_pickle(self.save_path, full_data)
+        elif self.save_path.endswith('.yaml') or self.save_path.endswith('.yml'):
+            full_data = ensure_numpy_as_list(full_data)
+            write_yaml(self.save_path, full_data)
+        elif self.save_path.endswith('.json'):
+            full_data = ensure_numpy_as_list(full_data)
+            write_json(self.save_path, full_data)
+        else:
+            raise Exception(f'[ERROR] Unsupported file format: {self.save_path}')
+
+####################################
 ## Functions for collecting demos
 ####################################
-def get_env_setup_data(cfg: dict, scene: Scene):
-    ## Panda
-    init_arm_q = scene.robot.arm.get_joint_positions()
-    init_gripper_q = scene.robot.gripper.get_joint_positions()
-    init_q = np.concatenate([init_arm_q, init_gripper_q])
-    init_robot_pos = scene.robot.arm.get_position()
-    init_robot_quat = scene.robot.arm.get_quaternion()
-    init_robot_quat = xyzw_to_wxyz(init_robot_quat)
-    
-    robot_name = 'Panda'
-    robot = Object.get_object(robot_name)
-    init_panda_pos = robot.get_position()
-    init_panda_quat = robot.get_quaternion()
-    init_panda_quat = xyzw_to_wxyz(init_panda_quat)
-    
-    assert np.allclose(init_robot_pos, init_panda_pos)
-    assert np.allclose(init_robot_quat, init_panda_quat)
-    
-    print(f'[DEBUG] Initial panda q: {float_array_to_str(init_q)}')
-    print(f'[DEBUG] Initial panda position: {float_array_to_str(init_panda_pos)}')
-    print(f'[DEBUG] Initial panda orientation: {float_array_to_str(init_panda_quat)}')
-    
-    data_env_setup = {
-        'init_q': init_q,
-        'init_robot_pos': init_robot_pos,
-        'init_robot_quat': init_robot_quat,
-        'init_target_pos': init_robot_pos + np.array([20,20,20]),    # XXX
-        'init_target_quat': init_robot_quat,  # XXX
-    }
-    
-    ## Objects
-    object_names = cfg['objects']
-    for object_name in object_names:
-        object = Object.get_object(object_name)
-        init_object_pos = object.get_position()
-        init_object_quat = object.get_quaternion()
-        init_object_quat = xyzw_to_wxyz(init_object_quat)
-        data_env_setup |= {
-            f'init_{object_name}_pos': init_object_pos,
-            f'init_{object_name}_quat': init_object_quat,
-        }
-        print(f'[DEBUG] Initial {object_name} position: {float_array_to_str(init_object_pos)}')
-        print(f'[DEBUG] Initial {object_name} orientation: {float_array_to_str(init_object_quat)}')
-
-    ## Joints
-    joint_names = cfg['joints']
-    for joint_name in joint_names:
-        joint = Joint(joint_name)
-        init_joint_q = joint.get_joint_position()
-        init_joint_q = np.array([init_joint_q])  # float to array
-        data_env_setup |= {
-            f'init_{joint_name}_q': init_joint_q,
-        }
-        print(f'[DEBUG] Initial {joint_name} q: {float_array_to_str(init_joint_q)}')
-    
-    return data_env_setup
-
-def get_demo_one_try(cfg: dict, scene: Scene, variation_index) -> Tuple[Demo, dict]:
-    scene.reset()
-    desc = scene.init_episode(variation_index, max_attempts=10)
-    data_env_setup = get_env_setup_data(cfg, scene)
-    demo = scene.get_demo(record=True)
-    return demo, data_env_setup
-
-def get_demo(cfg: dict, scene: Scene, variation_index) -> Tuple[Demo, dict]:
-    attempts = 10
-    error = None
-    while attempts > 0:
-        try:
-            demo, data_env_setup = get_demo_one_try(cfg, scene, variation_index)
-        except Exception as e:
-            attempts -= 1
-            print(f'[DEBUG] Failed to get task {scene.task.get_name()} (variation: {variation_index}). Rest attempts {attempts}. Retrying...')
-            error = e  # record the error
-            continue
-        break
-    
-    if attempts > 0:
-        print(f'[INFO] Successfully got task {scene.task.get_name()} (variation: {variation_index}), length: {len(demo)}')
-        return demo, data_env_setup
-    else:
-        print(f'[ERROR] Failed to get task {scene.task.get_name()} (variation: {variation_index})')
-        raise error
-
-def get_demos(cfg: dict, task: Task, scene: Scene, variation_num: int) -> List[Demo]:
-    task_name = task.get_name()
-    save_dir = mkdir(os.path.join('outputs', f'{task_name}'))
-    
-    scene.load(task)
-    scene.init_task()
-    
-    data_demos = []
-    for variation_index in range(variation_num):
-        demo, data_env_setup = get_demo(cfg, scene, variation_index)
-        data_demo = get_demo_data(demo, data_env_setup, task_name, variation_index)    
-        data_demos.append(data_demo)
-    
-    save_path = os.path.join(save_dir, f'{task_name}.pkl')
-    save_demos(data_demos, save_path)
-
-def get_demo_data(demo: Demo, data_env_setup: dict, task_name: str, variation_index: int):
-    data = {}
-
-    ## Process trajectory
-    qs = []
-    ee_acts = []
-    for obs in demo:
-        # collecting joint positions for q [L x q_len]
-        q = obs.joint_positions.tolist() + obs.gripper_joint_positions.tolist()
-        # collecting gripper open for ee_act [L x 1]
-        ee_act = [obs.gripper_open]
+class DemoGetter:
+    def __init__(self, cfg: dict, writer: DemoWriter, task_class):
+        self._launch_sim()
+        self.robot = Robot(Panda(), PandaGripper())
+        self._create_scene()
+        self._load_task(task_class)
         
-        qs.append(q)
-        ee_acts.append(ee_act)
+        self.cfg = cfg
+        self.writer = writer
     
-    data_traj = {
-        'q': np.array(qs),
-        'ee_act': np.array(ee_acts),
-        'success': np.ones(len(qs), dtype=bool)
-    }
+    def _launch_sim(self):
+        DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+        self.sim = PyRep()
+        ttt_file = os.path.join(
+            DIR_PATH, '..', 'rlbench', TTT_FILE)
+        self.sim.launch(ttt_file, headless=args.headless)
+        self.sim.step_ui()
+        self.sim.set_simulation_timestep(1/60)  # Control frequency 60Hz
+        self.sim.step_ui()
+        self.sim.start()
     
-    ## All demo data
-    data_demo = {
-        'name': f'{task_name}_traj_{variation_index}',
-        'asset_name': 'default_asset',
-        'episode_len': len(qs),
-        'env_setup': data_env_setup,
-        'robot_traj': data_traj
-    }
+    def _create_scene(self):
+        obs_config = ObservationConfig()
+        obs_config.joint_positions = True
+        obs_config.gripper_joint_positions = True
+        obs_config.gripper_open = True
+        self.scene = Scene(self.sim, self.robot, obs_config)
     
-    return data_demo
+    def _load_task(self, task_class):
+        self.task = task_class(self.sim, self.robot)
+        self.scene.load(self.task)
+        self.scene.init_task()
+    
+    def try_get_demo(self, variation_index):
+        self.scene.reset()
+        desc = self.scene.init_episode(variation_index, max_attempts=10)
+        self.writer.capture_env_setup_data(self.scene)
+        demo = self.scene.get_demo(record=True)
+        return demo
 
-def save_demos(data_demos: List[dict], save_path: str):
-    max_episode_len = max([data_demo['episode_len'] for data_demo in data_demos])
-    print(f'[INFO] Saving demos to {save_path}, max_episode_len: {max_episode_len}')
-    
-    full_data = {
-        'max_episode_len': max_episode_len,
-        'demos': {
-            'franka': data_demos
-        }
-    }
-    
-    if save_path.endswith('.pkl'):
-        full_data = data_numpy_float32(full_data)
-        write_pickle(save_path, full_data)
-    elif save_path.endswith('.yaml') or save_path.endswith('.yml'):
-        full_data = data_numpy_to_list(full_data)
-        write_yaml(save_path, full_data)
-    elif save_path.endswith('.json'):
-        full_data = data_numpy_to_list(full_data)
-        write_json(save_path, full_data)
-    else:
-        raise Exception(f'[ERROR] Unsupported file format: {save_path}')
+    def get_demo(self, variation_index):
+        attempts = 10
+        error = None
+        while attempts > 0:
+            try:
+                demo = self.try_get_demo(variation_index)
+            except Exception as e:
+                attempts -= 1
+                print(f'[DEBUG] Failed to get task {self.scene.task.get_name()} (variation: {variation_index}). Rest attempts {attempts}. Retrying...')
+                error = e  # record the error
+                continue
+            break
+        
+        if attempts > 0:
+            print(f'[INFO] Successfully got task {self.scene.task.get_name()} (variation: {variation_index}), length: {len(demo)}')
+            return demo
+        else:
+            print(f'[ERROR] Failed to get task {self.scene.task.get_name()} (variation: {variation_index})')
+            raise error
+
+    def get_demos(self, num_variations: int):
+        task_name = self.task.get_name()
+        
+        for variation_index in range(num_variations):
+            demo = self.get_demo(variation_index)
+            self.writer.save_demo_data(demo, task_name, variation_index)    
+        
+        self.writer.write()
 
 ####################################
 ## Main
@@ -265,26 +313,8 @@ if __name__ == '__main__':
         raise RuntimeError('Could not find the task file: %s' % python_file)
     task_class = task_file_to_task_class(args.task)
     
-    ## CoppeliaSim
-    DIR_PATH = os.path.dirname(os.path.abspath(__file__))
-    sim = PyRep()
-    ttt_file = os.path.join(
-        DIR_PATH, '..', 'rlbench', TTT_FILE)
-    sim.launch(ttt_file, headless=args.headless)
-    sim.step_ui()
-    sim.set_simulation_timestep(1/60)  # Control frequency 60Hz
-    sim.step_ui()
-    sim.start()
-    
-    ## Setup
-    robot = Robot(Panda(), PandaGripper())
-    active_task = task_class(sim, robot)
-    obs_config = ObservationConfig()
-    obs_config.joint_positions = True
-    obs_config.gripper_joint_positions = True
-    obs_config.gripper_open = True
-    scene = Scene(sim, robot, obs_config)
-    
     ## Run task
-    demos = get_demos(cfg, active_task, scene, args.variation_num)
-    
+    save_dir = mkdir(os.path.join('outputs', task_name))
+    writer = DemoWriter(cfg, os.path.join(save_dir, f'{task_name}.pkl'))
+    getter = DemoGetter(cfg, writer, task_class)
+    getter.get_demos(args.variation_num)
