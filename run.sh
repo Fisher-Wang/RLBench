22f6463ca28ad98742ff67d91c097e291b5e9fbf
Yuyang Li <i@aidenli.net>
2024-07-08 18:06:24 +0800

    [update] updated validation code to include all cameras

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ mkdir -p logs/
 # for task in put_toilet_roll_on_stand hang_frame_on_hanger stack_wine put_knife_in_knife_block take_frame_off_hanger get_ice_from_fridge meat_off_grill meat_on_grill hockey;
 # for task in phone_on_base put_all_groceries_in_cupboard in put_bottle_in_fridge place_cups empty_container put_plate_in_colored_dish_rack put_item_in_drawer take_item_out_of_drawer put_knife_on_chopping_board;
 # for task in set_the_table put_books_on_bookshelf put_groceries_in_cupboard put_money_in_safe put_rubbish_in_bin setup_checkers setup_chess solve_puzzle stack_chairs;
+
+for task in meat_off_grill meat_on_grill hockey;
 do
     # python tools/collect_demo.py --episode_num=500 --headless --task=$task --record_object_states
     nohup python tools/replay_object_states.py --headless --task $task > logs/$task.out &
--- validate_3d.py
+++ validate_3d.py
@@ -8,22 +8,17 @@ import numpy as np
 
 task_name = "basketball_in_hoop"
 demo_id = 0
-load_dir = f"outputs/{task_name}/norobot_replay/{str(demo_id).zfill(4)}"
+load_dir = f"outputs/{task_name}/norobot_replay/demo_{str(demo_id).zfill(4)}"
 metadata = json.load(open(f"outputs/{task_name}/norobot_replay/metadata.json", 'r'))
 
-cam_name = "front" # "over_shoulder_left" "over_shoulder_left"
+cam_intr = {}
+rgb_in, depth_in = {}, {}
 
-cam_intr_mat = np.array(metadata[f"cam_intr_{cam_name}"])
-cam_extr_mat = np.array(metadata[f"cam_extr_{cam_name}"])
-rgb_data = cv2.VideoCapture(os.path.join(load_dir, f"{cam_name}_rgb.mp4"))
-depth_data = cv2.VideoCapture(os.path.join(load_dir, f"{cam_name}_depth.mkv"))
+width, height = 512, 512
 
-while rgb_data.isOpened:
-    (rgb_ret, rgb_frame), (depth_ret, depth_frame) = rgb_data.read(), depth_data.read()
-    width, height = rgb_frame.shape
-    
-    rgbd_frame = o3d.geometry.create_rgbd_image_from_color_and_depth(rgb_frame, depth_frame, depth_scale=1.0)
-    intr = o3d.camera.PinholeCameraIntrinsic(
+for cam_name in ["cam_front", "cam_over_shoulder_left", "cam_over_shoulder_left"]:
+    cam_intr_mat = np.array(metadata[f"cam_intr_{cam_name}"])        
+    cam_intr[cam_name] = o3d.camera.PinholeCameraIntrinsic(
         width=int(width),
         height=int(height),
         fx=cam_intr_mat[0, 0],
@@ -31,6 +26,30 @@ while rgb_data.isOpened:
         cx=cam_intr_mat[0, 2],
         cy=cam_intr_mat[1, 2],
     )
-    point_cloud = o3d.geometry.create_point_cloud_from_rgbd_image(rgbd_frame, intr, cam_extr_mat)
-    o3d.visualization.draw([point_cloud])
+
+    rgb_in[cam_name] = cv2.VideoCapture(os.path.join(load_dir, f"{cam_name}_rgb.mp4"))
+    depth_in[cam_name] = cv2.VideoCapture(os.path.join(load_dir, f"{cam_name}_depth.mkv"))
+    depth_in[cam_name].set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('Y','1','6',' '))
+    depth_in[cam_name].set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
+
+while rgb_in['cam_front'].isOpened:
+    pc_o3d_frame = []
+    for cam_name in ["cam_front", "cam_over_shoulder_left", "cam_over_shoulder_left"]:
+        (rgb_ret, rgb_frame), (depth_ret, depth_frame) = rgb_in[cam_name].read(), depth_in[cam_name].read()
+        
+        rgb_o3d = o3d.geometry.Image(np.ascontiguousarray(rgb_frame).astype(np.uint8))
+        depth_o3d = o3d.geometry.Image(np.ascontiguousarray(depth_frame / 65535).astype(np.float32))
+        rgbd_frame = o3d.geometry.RGBDImage.create_from_color_and_depth(rgb_o3d, depth_o3d, depth_scale=1.0)
+        
+        # TODO: Fix this: the extrinsics matrix changes over time
+        cam_extr = np.array(metadata[f"cam_extr_{cam_name}"])
+        
+        point_cloud = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_frame, cam_intr[cam_name], cam_extr)
+        pc_o3d_frame.append(point_cloud)
+        
+    o3d.visualization.draw(pc_o3d_frame)
     
+    for pc in pc_o3d_frame:
+        del pc
+    del pc_o3d_frame
+        
